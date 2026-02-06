9dfc47940fe61365987f18d823c720963c728823
tokeiya3@Home <net_seed@hotmai.com>
2026-02-06 23:58:53 +0900

    Add `add_assume` procedural macro scaffolding to `proc_macro` crate

---
+++ proc_macro/src/add_assume.rs
@@ -0,0 +1,3 @@
+pub fn add_assume(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+	todo!()
+}
--- proc_macro/src/lib.rs
+++ proc_macro/src/lib.rs
@@ -1,11 +1,16 @@
+mod add_assume;
 mod add_check;
 mod character_manipulator;
 
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, Item, ItemEnum};
-
+use syn::{Item, ItemEnum, parse_macro_input};
 
 #[proc_macro_attribute]
 pub fn add_check(_: TokenStream, input: TokenStream) -> TokenStream {
 	add_check::add_check(input)
+}
+
+#[proc_macro_attribute]
+pub fn add_assume(_: TokenStream, input: TokenStream) -> TokenStream {
+	add_assume::add_assume(input)
 }
