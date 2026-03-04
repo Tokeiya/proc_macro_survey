25ba730ff3400ea677bce14943360876b9f1b619
tokeiya3@Home <net_seed@hotmai.com>
2026-03-04 21:35:35 +0900

    Sync dependencies and refactor `to_snake` implementation with regex-based transformations and lazy initialization for performance optimization.

--- code_generator/src/bin/gen_assert.rs
+++ code_generator/src/bin/gen_assert.rs
@@ -1,3 +1,2 @@
 use code_generator::to_snake;
-
 fn main() {}
--- code_generator/src/to_snake.rs
+++ code_generator/src/to_snake.rs
@@ -1,12 +1,40 @@
+use regex::Regex;
 use std::borrow::Cow;
+use std::sync::LazyLock;
 
+static IS_SNAKE: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"^[a-z]+(_[a-z0-9]+)*$").unwrap());
+static CONTAIN_OTHER: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"[^(A-Za-z0-9_)]+$").unwrap());
 pub fn convert(scr: &str) -> Cow<'_, str> {
-	todo!()
+	if CONTAIN_OTHER.is_match(scr) || IS_SNAKE.is_match(scr) || scr.is_empty() {
+		Cow::from(scr)
+	} else {
+		let mut buff = String::new();
+		let mut iter = scr.chars();
+
+		buff.push(iter.next().unwrap().to_ascii_lowercase());
+
+		for c in iter {
+			if c.is_ascii_uppercase() {
+				buff.push('_');
+				buff.push(c.to_ascii_lowercase());
+			} else {
+				buff.push(c);
+			}
+		}
+
+		Cow::from(buff)
+	}
 }
 
 #[cfg(test)]
 mod tests {
 	use super::*;
+
+	#[test]
+	fn foo() {
+		let a = IS_SNAKE.is_match("Aaa");
+		dbg!(a);
+	}
 
 	#[test]
 	fn convert_from_snake() {
