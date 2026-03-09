7179ffd700b723021e0156b357245e2cffeed283
tokeiya3@Home <net_seed@hotmai.com>
2026-03-09 15:28:42 +0900

    sync

---
+++ code_generator/src/gen_assume.rs
@@ -0,0 +1,21 @@
+use syn::{Error as SynError, Fields, ItemEnum, Result as SynResult, Variant, parse_quote};
+
+fn gen_function_body(variant: &Variant, enum_ident: &syn::Ident) -> proc_macro2::TokenStream {
+	let ident = &variant.ident;
+
+	match &variant.fields {
+		Fields::Named(named) => {}
+		Fields::Unnamed(unnamed) => {}
+		Fields::Unit => {}
+	}
+
+	todo!()
+}
+
+pub fn gen_assume(src: &str, attributes: &[&str]) -> SynResult<String> {
+	let ast = syn::parse_str::<ItemEnum>(src)?;
+	let ident = ast.ident;
+	let variant = ast.variants;
+
+	todo!()
+}
--- code_generator/src/lib.rs
+++ code_generator/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod gen_assert;
+mod gen_assume;
 pub mod gen_impl;
 pub mod to_snake;
